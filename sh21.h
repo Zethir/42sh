11146446329cd98366ce10f70fe9ccb326690038
Quentin DIAZ <qdiaz@e1r3p16.42.fr>
2016-07-14 17:37:54 +0200

    toto

--- sh21.h
+++ sh21.h
@@ -6,7 +6,7 @@
 /*   By: cboussau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/12 11:44:03 by cboussau          #+#    #+#             */
-/*   Updated: 2016/07/14 15:10:06 by qdiaz            ###   ########.fr       */
+/*   Updated: 2016/07/14 17:36:55 by qdiaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,6 +118,7 @@ int					deal_with_pipe(t_struct *info, char *arg);
 int					start_pipe(int pipefds[], int num);
 int					check_for_chevron(t_struct *ptr);
 int					check_for_parenth(char *arg);
+int					do_echo(char **cmd);
 char				**deal_with_opt(t_struct *info, char **arg);
 char				**malloc_tab(char **arg);
 char				*deal_with_termcap(t_struct *info);
@@ -125,6 +126,5 @@ char				**split_path(t_lst *node);
 char				*check_path(char **path, char *arg);
 char				**get_env(t_lst *node);
 char				**save_command(t_struct *ptr);
-int					do_echo(char **cmd);
 
 #endif
